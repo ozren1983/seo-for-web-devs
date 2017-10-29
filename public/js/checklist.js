95563804faba74f02a3e746490788d2e34acccc3
Ozren Lapcevic <ozren.lapcevic@gmail.com>
2017-10-29 19:27:43 +0100

    Add more items to checklist

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -195,36 +195,36 @@ function isObject(val) {
 const store = __webpack_require__(2)
 
 const checklist = {
-    // add single click event listener on the whole checklist
-    init: () => {
-        document.getElementById('checklist').addEventListener('click', function(event) {
-            checklist.processClick(event.target)
-        })
-    },
-    // process click on labels and inputs, and ignore all other elements
-    processClick: (target) => {
-        if (target.nodeName === 'LABEL') {
-            checklist.writeToStore(target.previousSibling.id, target.previousSibling.checked)
-        }
-        else if (target.nodeName === 'INPUT') {
-            checklist.writeToStore(target.id, target.checked)
-        }
-    },
-    readFromStore: () => {
-        store.each((value, key) => {
-            if (value === true) {
-                document.getElementById(key).checked = true
-            }
-        })
-    },
-    writeToStore: (id, value) => {
-        store.set(`${id}`, value)
+  // add single click event listener on the whole checklist
+  init: () => {
+    document.getElementById('checklist').addEventListener('click', (event) => {
+      checklist.processClick(event.target)
+    })
+  },
+  // process click on labels and inputs, and ignore all other elements
+  processClick: (target) => {
+    if (target.nodeName === 'LABEL') {
+      checklist.writeToStore(target.previousSibling.id, target.previousSibling.checked)
     }
+    else if (target.nodeName === 'INPUT') {
+      checklist.writeToStore(target.id, target.checked)
+    }
+  },
+  readFromStore: () => {
+    store.each((value, key) => {
+      if (value === true) {
+        document.getElementById(key).checked = true
+      }
+    })
+  },
+  writeToStore: (id, value) => {
+    store.set(`${id}`, value)
+  }
 }
 
 window.addEventListener('load', () => {
-    checklist.init()
-    checklist.readFromStore()
+  checklist.init()
+  checklist.readFromStore()
 })
 
 
