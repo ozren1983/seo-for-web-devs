e9a6392ceb9134b2c18f7609275a38aedaa5ab27
Ozren Lapcevic <ozren.lapcevic@gmail.com>
2017-10-31 19:44:06 +0100

    Add Loader.io verification

---
+++ controllers/loaderIoController.js
@@ -0,0 +1,4 @@
+exports.verification = (req, res) => {
+  res.send('loaderio-e1e64128a587d2cbe4d34a04c17eba88')
+}
+
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const checklistController = require('../controllers/checklistController.js')
 const homeController = require('../controllers/homeController.js')
+const loaderIoController = require('../controllers/loaderIoController.js')
 
 const router = express.Router()
 
@@ -11,5 +12,8 @@ router.get('/', homeController.homepage)
 
 // Checklist
 router.get('/checklist', checklistController.checklist)
+
+// Loader.io verification
+router.get('/loaderio-e1e64128a587d2cbe4d34a04c17eba88.txt', loaderIoController.verification)
 
 module.exports = router
